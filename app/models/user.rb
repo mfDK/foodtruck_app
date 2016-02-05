099d08107f2f4f3a4e3312cce489740159f83af4
Christopher Mora <cmora09@gmail.com>
2016-02-05 14:52:06 -0500

    Created geocode for address. Address turned into lat and long confirmed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
 	has_many :orders
 	# This is from the orders table, possibly need to
 	# made a has_many :through **
+
+	geocoded_by :address
+
+	# checks to see if an address has been changed/updated or is present before using geocode
+	after_validation :geocode, if: ->(obj){ obj.street_address.present? and obj.state.present? and obj.city.present? and obj.zip.present? and obj.street_address_changed? and obj.state_changed? and obj.city_changed? and obj.zip_changed?}
+
+	def address
+		[city, state, zip, street_address].compact.join(",")
+	end
+
 end
