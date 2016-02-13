36ef973a25df43d7020271de89431609fedde35c
Danny Kim <dkim7474@gmail.com>
2016-02-13 17:26:54 -0500

    Added commment to the foodorder controller. closes #14.

--- app/controllers/foodorders_controller.rb
+++ app/controllers/foodorders_controller.rb
@@ -1,2 +1,9 @@
 class FoodordersController < ApplicationController
+	def create
+		# order_id should be filled in by the params when a new order is created
+		# a button will create the relationship between an order and a food item
+		# that belongs to the truck
+	end
+	def destroy
+	end
 end
